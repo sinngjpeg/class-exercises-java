424461967c07f3328c607410a99c8c527cc10402
Ingrid Silva <ingridsilvatec@outlook.com>
2020-08-12 17:07:39 -0300

    exercicio-conta-bancaria

--- controle-estoque/src/application/Program.java
+++ controle-estoque/src/application/Program.java
@@ -16,17 +16,14 @@ public class Program {
 		String name = sc.nextLine();
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		int quantity = sc.nextInt();
-
-		Product product = new Product(name, price, quantity);
+		Product product = new Product(name, price );
 
 		System.out.println();
 		System.out.println("Product data : " + product.toString());
 
 		System.out.println();
 		System.out.println("Enter the number of products to be added in stock: ");
-		quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		product.addProducts(quantity);
 
 		System.out.println();
--- controle-estoque/src/entities/Product.java
+++ controle-estoque/src/entities/Product.java
@@ -2,9 +2,9 @@ package entities;
 
 public class Product {
 
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 	
 	public Product(String name, double price, int quantity) {
 		this.name = name; 
@@ -12,6 +12,37 @@ public class Product {
 		this.quantity = quantity;
 		
 	}
+	
+	public Product(String name, double price) {
+		this.name = name; 
+		this.price = price;
+				
+	}
+	
+
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	
 
 	public double totalValueInStock() {
 		return price * quantity;
