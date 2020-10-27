c34e4fb8784097c5811009be277fa575a3ef1aef
Ingrid Silva <ingridsilvatec@outlook.com>
2020-10-27 15:44:23 -0300

    exercicio Aluguel de carros sem interface

---
+++ exercicioAluguelDeCarro/src/model/services/RentalService.java
@@ -0,0 +1,36 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerHour;
+
+	private BrazilTexService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerhour, BrazilTexService taxService) {
+		super();
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerhour;
+		this.taxService = taxService;
+	}
+
+	// Responsavel por gerar a nota
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
+
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
