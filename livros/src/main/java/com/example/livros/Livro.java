77e97827b1d8bf42311a14cae6e4e9b4d00e67c9
Ingrid Silva <ingridsilvatec@outlook.com>
2020-10-29 11:26:42 -0300

    Test deveRetornarUmLivro

--- livros/src/main/java/com/example/livros/Livro.java
+++ livros/src/main/java/com/example/livros/Livro.java
@@ -1,4 +1,16 @@
 package com.example.livros;
 
 public class Livro {
+
+
+
+
+    public void setId(int id) {
+    }
+
+    public void setNome(String nome) {
+    }
+
+    public void setAutor(String autor) {
+    }
 }
--- livros/src/test/java/com/example/livros/LivrosTest.java
+++ livros/src/test/java/com/example/livros/LivrosTest.java
@@ -8,6 +8,9 @@ public class LivrosTest {
     @Test
     public void deveCriarUmLivro(){
         Livro livro = new Livro();
+        livro.setId(1);
+        livro.setNome("Iracema");
+        livro.setAutor("José de Alencar");
 
     }
 }
