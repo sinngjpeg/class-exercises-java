24f07d7271d0088959890b27d32a69b8e5686314
Ingrid Silva <ingridsilvatec@outlook.com>
2020-10-29 11:14:05 -0300

    API dos Livros TDD - teste 1 - cria um carro

---
+++ livros/src/main/java/com/example/livros/Livro.java
@@ -0,0 +1,4 @@
+package com.example.livros;
+
+public class Livro {
+}
---
+++ livros/src/test/java/com/example/livros/LivrosTest.java
@@ -0,0 +1,13 @@
+package com.example.livros;
+
+import org.junit.jupiter.api.Test;
+
+public class LivrosTest {
+
+
+    @Test
+    public void deveCriarUmLivro(){
+        Livro livro = new Livro();
+
+    }
+}
