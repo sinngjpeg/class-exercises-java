b46db33efc1132fe8e2d5c5e70e43ed5ea129e47
Ingrid Silva <ingridsilvatec@outlook.com>
2020-10-29 11:33:39 -0300

    Test deveRetornarUmLivro

--- livros/src/main/java/com/example/livros/Livro.java
+++ livros/src/main/java/com/example/livros/Livro.java
@@ -2,15 +2,26 @@ package com.example.livros;
 
 public class Livro {
 
+    private Long id;
+    private String nome;
+    private String autor;
 
 
-
-    public void setId(int id) {
+    public void setId(Long id) {
     }
 
     public void setNome(String nome) {
     }
 
     public void setAutor(String autor) {
+    }
+
+    public void getId(Long id) {
+    }
+
+    public void getNome(String nome) {
+    }
+
+    public void getAutor(String autor) {
     }
 }
--- livros/src/test/java/com/example/livros/LivrosTest.java
+++ livros/src/test/java/com/example/livros/LivrosTest.java
@@ -8,9 +8,17 @@ public class LivrosTest {
     @Test
     public void deveCriarUmLivro(){
         Livro livro = new Livro();
-        livro.setId(1);
+        livro.setId((long) 1);
         livro.setNome("Iracema");
         livro.setAutor("José de Alencar");
 
+    }
+
+    @Test
+    public void deveRetornarUmLivro(){
+        Livro livro = new Livro();
+        livro.getId((long) 1);
+        livro.getNome("Iracema");
+        livro.getAutor("José de Alencar");
     }
 }
