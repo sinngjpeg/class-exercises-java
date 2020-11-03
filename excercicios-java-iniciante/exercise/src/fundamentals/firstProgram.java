04ee8de74cb2bdfa3b6082e47b2bf28c73b092ef
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-03 18:00:26 -0300

    javadoc, constantes, variaveis, comentarios

--- excercicios-java-iniciante/exercise/src/fundamentals/firstProgram.java
+++ excercicios-java-iniciante/exercise/src/fundamentals/firstProgram.java
@@ -1,8 +1,22 @@
 package fundamentals;
 
-public class firstProgram {
+public class FirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+
+		/*
+		 * crtl + shift + f11 - run application 
+		 * crtl + shift + F - format code 
+		 * crtl + shift + O - format imports 
+		 * sysout - system.out.print
+		 * 
+		 */
+		
+		/**
+		 * @author ingrid.da.silva 
+		 * @since JDK 1.0
+		 * 
+		 */
 	}
 }
