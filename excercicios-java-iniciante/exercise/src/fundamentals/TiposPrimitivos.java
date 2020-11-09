b097c01db4ab63e54d8099d69c020b2134dffec0
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-09 11:46:55 -0300

    tipos primitivos

---
+++ excercicios-java-iniciante/exercise/src/fundamentals/TiposPrimitivos.java
@@ -0,0 +1,43 @@
+package fundamentals;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+
+		// Informações do funcionario
+
+		// Tipos numericos inteiros
+		
+		byte anosdeEmpresa = 23;
+		short numeroDeVoos = 542;
+		int id = 56789;
+		long pontosAcumulados = 3_234_845_223L;
+		
+		// Tipos numericos reais 
+		
+		float salario = 11_445.44F;
+		double vendasAcumuladas = 2_991_797_103.01;
+		
+		// Tipos booleno
+		boolean estaDeFerias = false; //true
+		
+		//Tipo caractere
+		char status = 'A'; //ativo 
+		
+		
+		//Dias de empresa 
+		System.out.println(anosdeEmpresa * 365);
+		
+		//Numero de viagens 
+		System.out.println(numeroDeVoos / 2);
+		
+		//Pontos por real 
+		System.out.println(pontosAcumulados / vendasAcumuladas);
+		
+		System.out.println(id + ": ganha ->" + salario);
+		
+		
+
+	}
+
+}
