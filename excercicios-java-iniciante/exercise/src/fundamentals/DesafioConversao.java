9b5a28382b034f3c74321da3875905d7f90ec170
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-10 20:03:01 -0300

    operadores aritmeticos

--- excercicios-java-iniciante/exercise/src/fundamentals/DesafioConversao.java
+++ excercicios-java-iniciante/exercise/src/fundamentals/DesafioConversao.java
@@ -5,25 +5,24 @@ import java.util.Scanner;
 public class DesafioConversao {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Informe o primeiro salario: ");
 		String valor1 = sc.next().replace(",", ".");
-		
+
 		System.out.print("\nInforme o segundo salario: ");
 		String valor2 = sc.next().replace(",", ".");
 		System.out.print("\nInforme o terceiro salario: ");
 		String valor3 = sc.next().replace(",", ".");
-		
+
 		double salario1 = Double.parseDouble(valor1);
 		double salario2 = Double.parseDouble(valor2);
 		double salario3 = Double.parseDouble(valor3);
-		
-		
-		double media = (salario1 + salario2 +  salario3) /3;
-		System.out.println("A media dos salarios é" + media);
+
+		double media = (salario1 + salario2 + salario3) / 3;
+		System.out.println("A media dos salarios é " + media);
 		sc.close();
 	}
-	
+
 }
