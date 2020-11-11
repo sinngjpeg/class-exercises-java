0eeffb7657a645da52feae3f5f548f386bedd3f6
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-11 17:29:58 -0300

    Operadores Ternarios

---
+++ excercicios-java-iniciante/exercise/src/fundamentals/operadores/Ternario.java
@@ -0,0 +1,23 @@
+package fundamentals.operadores;
+
+public class Ternario {
+
+	public static void main(String[] args) {
+
+		double media = 7.6;
+
+		String resultadoFinal = media >= 7.0 ? "Aprovado." : "em recuperação.";
+
+		System.out.println("O aluno está: " + resultadoFinal);
+		
+		double nota = 9.9;
+		boolean bomComportamento = false;
+		boolean passouPorMedia = nota >= 7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		String resultado = temDesconto ? "Sim." : "Nao.";
+		
+		System.out.printf("Team desconto? %s",  resultado);
+
+	}
+
+}
