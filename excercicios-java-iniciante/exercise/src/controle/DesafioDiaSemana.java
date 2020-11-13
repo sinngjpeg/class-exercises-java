c5f79ac031b13bb8fa761831da8f815299f6e8ca
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-13 17:30:42 -0300

    estrutura while

--- excercicios-java-iniciante/exercise/src/controle/DesafioDiaSemana.java
+++ excercicios-java-iniciante/exercise/src/controle/DesafioDiaSemana.java
@@ -15,7 +15,7 @@ public class DesafioDiaSemana {
 			System.out.println(1);
 		} else if ("segunda".equalsIgnoreCase(dia)) {
 			System.out.println(2);
-		} else if ("terca".equalsIgnoreCase(dia)) {
+		} else if ("terca".equalsIgnoreCase(dia) || "terça".equalsIgnoreCase(dia)) {
 			System.out.println(3);
 		} else if ("quarta".equalsIgnoreCase(dia)) {
 			System.out.println(4);
@@ -23,8 +23,10 @@ public class DesafioDiaSemana {
 			System.out.println(5);
 		} else if ("sexta".equalsIgnoreCase(dia)) {
 			System.out.println(6);
-		} else if ("sabado".equalsIgnoreCase(dia)) {
+		} else if ("sabado".equalsIgnoreCase(dia) || "sábado".equalsIgnoreCase(dia)) {
 			System.out.println(7);
+		} else {
+			System.out.println("Dia inválido!");
 		}
 
 		sc.close();
