36559cdcc61c10112ea2ba22f2cc1bbb33d737da
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-17 02:12:59 -0300

    Break Rotulado

--- excercicios-java-iniciante/exercise/src/controle/Continue.java
+++ excercicios-java-iniciante/exercise/src/controle/Continue.java
@@ -11,13 +11,12 @@ public class Continue {
 			System.out.println(i);
 		}
 
-	
-
-	for (int i = 0; i < 10; i++) {
-		if (i == 5) {
-			continue;
+		for (int i = 0; i < 10; i++) {
+			if (i == 5) {
+				continue;
+			}
+			System.out.println(i);
 		}
-		System.out.println(i);
-	}
 
-}}
+	}
+}
