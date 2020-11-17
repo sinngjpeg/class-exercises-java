300aa0380fd12947ab53dc68546191e4d8b4c110
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-17 02:17:56 -0300

    Continue Rotulado

--- excercicios-java-iniciante/exercise/src/controle/ContinueRotulado.java
+++ excercicios-java-iniciante/exercise/src/controle/ContinueRotulado.java
@@ -1,13 +1,13 @@
 package controle;
 
-public class BreakRotulado {
+public class ContinueRotulado {
 
 	public static void main(String[] args) {
 
 		externo: for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if (i == 1) {
-					break externo;
+					continue externo;
 				}
 				System.out.printf("%d %d ", i, j);
 			}
