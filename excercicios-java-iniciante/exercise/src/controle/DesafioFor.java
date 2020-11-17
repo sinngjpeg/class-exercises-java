edcab3b68fa6eeaa4bee879a286538adbd4fbd15
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-17 01:20:50 -0300

    Desafio For

---
+++ excercicios-java-iniciante/exercise/src/controle/DesafioFor.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class DesafioFor {
+
+	public static void main(String[] args) {
+		
+		
+		for (String v = "#"; !v.equals("######"); v += "#") {
+			System.out.println(v);
+		
+		}
+
+	}
+		//Desafio Nao pode usar valor numerico para controlar o laço!
+}
